59ae0020f151a443f4db2593685f688740abc66f
clement4saunier <clement4saunier@gmail.com>
2023-04-20 16:23:10 +0200

    [Add] links to projects

--- src/components/cards/ImageCard.tsx
+++ src/components/cards/ImageCard.tsx
@@ -1,13 +1,14 @@
 import type { Component } from "solid-js";
 import styles from "./ImageCard.module.css";
 
-const ImageCard: Component = ({ children, footer, src }) => (
-  <div className={["card", styles.layout].join(" ")}>
+const ImageCard: Component = ({ children, footer, src, corner }) => (
+  <div class={["card", styles.layout].join(" ")}>
     <img alt="icon" src={src}></img>
-    <div className={styles.content}>
+    <div class={styles.content}>
       <div>{children}</div>
       <div>{footer}</div>
     </div>
+    <div class={styles.corner}>{corner}</div>
   </div>
 );
 
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -72,31 +72,6 @@ const Home: Component = () => {
               </p>
             </div>
           </div>
-          {/* <Profile
-            ref={liftedProfileRef}
-            footer={
-              <>
-                <form action="https://www.linkedin.com/in/cl%C3%A9ment-saunier-211453156/" target="_blank">
-                  <button>LinkedIn</button>
-                </form>
-              </>
-            }
-          >
-            <div>blockchain</div>
-            <h1>Clément SAUNIER</h1>
-            <p>
-              Hi 👋, I'm a fullstack blockchain developer with a background in
-              game development. I dug deep into Ethereum and blockchain
-              technology in 2021 by building a decentralized project from
-              scratch, and am now a strong believer in the power of
-              decentralized open-source cryptography to build a trusted and
-              private online realm.
-              <br />
-              <br />
-              If you're looking for someone to work on your exciting project,
-              please contact me trough:
-            </p>
-          </Profile> */}
           <div>
 
             <p class={styles.brief} style={{ "text-align": "center" }}>
@@ -170,6 +145,11 @@ const Home: Component = () => {
         <ImageCard
           src={websiteLogo}
           footer={<Tags values={["Typescript", "Solid.js"]} />}
+          corner={
+            <button onClick={() => window.open("https://github.com/clement4saunier/DigitalHome")}>
+              <svg fill="currentColor" stroke-width="0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" height="1em" width="1em" style="overflow: visible;"><path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path></svg>
+            </button>
+          }
         >
           <h2>This website</h2>
           <p>Portfolio website</p>
@@ -194,6 +174,11 @@ const Home: Component = () => {
         <ImageCard
           src={quickstarterLogo}
           footer={<Tags values={["Javascript", "React.js", "Ipfs"]} />}
+          corner={
+            <button onClick={() => window.open("https://github.com/clement4saunier/FUNDSARESAFE")}>
+              <svg fill="currentColor" stroke-width="0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" height="1em" width="1em" style="overflow: visible;"><path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path></svg>
+            </button>
+          }
         >
           <h2>Quickstarter</h2>
           <p>
@@ -204,6 +189,11 @@ const Home: Component = () => {
         <ImageCard
           src={defactoLogo}
           footer={<Tags values={["Javascript", "React.js", "Ipfs"]} />}
+          corner={
+            <button onClick={() => window.open("https://github.com/clement4saunier/defacto-dapp")}>
+              <svg fill="currentColor" stroke-width="0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" height="1em" width="1em" style="overflow: visible;"><path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path></svg>
+            </button>
+          }
         >
           <h2>Defacto</h2>
           <p>
@@ -229,6 +219,11 @@ const Home: Component = () => {
         <ImageCard
           src={breakfirstLogo}
           footer={<Tags values={["C#", "Unity"]} />}
+          corner={
+            <button onClick={() => window.open("https://www.linkedin.com/company/breakfirst/about/")}>
+              <svg fill="currentColor" stroke-width="0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" height="1em" width="1em" style="overflow: visible;"><path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM349.3 793.7H230.6V411.9h118.7v381.8zm-59.3-434a68.8 68.8 0 1 1 68.8-68.8c-.1 38-30.9 68.8-68.8 68.8zm503.7 434H675.1V608c0-44.3-.8-101.2-61.7-101.2-61.7 0-71.2 48.2-71.2 98v188.9H423.7V411.9h113.8v52.2h1.6c15.8-30 54.5-61.7 112.3-61.7 120.2 0 142.3 79.1 142.3 181.9v209.4z"></path></svg>
+            </button>
+          }
         >
           <h2>Unity developer @ BreakFirst</h2>
           <p>
@@ -244,6 +239,11 @@ const Home: Component = () => {
             <Tags
               values={["Javascript", "Solidity", "React.js", "Next.js", "Ipfs"]}
             />
+          }
+          corner={
+            <button onClick={() => window.open("https://www.linkedin.com/company/b2expand/about/")}>
+              <svg fill="currentColor" stroke-width="0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" height="1em" width="1em" style="overflow: visible;"><path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM349.3 793.7H230.6V411.9h118.7v381.8zm-59.3-434a68.8 68.8 0 1 1 68.8-68.8c-.1 38-30.9 68.8-68.8 68.8zm503.7 434H675.1V608c0-44.3-.8-101.2-61.7-101.2-61.7 0-71.2 48.2-71.2 98v188.9H423.7V411.9h113.8v52.2h1.6c15.8-30 54.5-61.7 112.3-61.7 120.2 0 142.3 79.1 142.3 181.9v209.4z"></path></svg>
+            </button>
           }
         >
           <h2>Solidity/React developer @ B2Expand</h2>
